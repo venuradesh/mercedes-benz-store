5a3a4b0cf940a18072b9cc68e70269ff7809f33e
Venura Warnasooriya <venurawarnasooriya@gmail.com>
2022-04-16 14:25:29 +0530

    animations added

--- index.js
+++ index.js
@@ -112,3 +112,39 @@ function scrollActive() {
 }
 
 window.addEventListener("scroll", scrollActive);
+
+// scroll reveal animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2400,
+  delay: 400,
+  reset: false,
+});
+
+// sr2.reveal(".nav__logo", { origin: "left", opacity: 0 });
+// sr2.reveal(".nav__menu", { origin: "right", opacity: 0 });
+sr.reveal(".title-header .title");
+sr.reveal(".title-header .sub-title", { delay: 500 });
+sr.reveal(".title-header .desc", { delay: 600 });
+sr.reveal(".preview", { delay: 900 });
+sr.reveal(".performance", { delay: 1200, origin: "bottom" });
+sr.reveal(".button", { delay: 1500, origin: "bottom", opacity: 0, distance: "120px" });
+sr.reveal(".about__image img", { origin: "left", duration: 3000 });
+sr.reveal(".about__image .image__description", { delay: 200, origin: "bottom" });
+sr.reveal(".about__expressiveness", { delay: 500, origin: "right" });
+sr.reveal(".about__tradition", { delay: 700, origin: "left" });
+sr.reveal(".popular__title", { delay: 400, origin: "bottom" });
+sr.reveal(".swiper-wrapper", { delay: 600, origin: "bottom" });
+sr.reveal(".features__image", { delay: 400, origin: "bottom", distance: "0px", opacity: 0 });
+sr.reveal(".features__card-map", { delay: 600, origin: "bottom" });
+sr.reveal(".feature__card1", { delay: 1200, origin: "left" });
+sr.reveal(".feature__card2", { delay: 1200, origin: "right" });
+sr.reveal(".feature__card3", { delay: 1200, origin: "left" });
+sr.reveal(".featured__heading", { delay: 400, origin: "bottom" });
+sr.reveal(".featured__filter-section", { delay: 700, origin: "bottom" });
+sr.reveal(".featured__card", { delay: 1000, duration: 4000, opacity: 0, distance: "0px" });
+sr.reveal(".logo__item", { delay: 400, duration: 4000, origin: "bottom" });
+sr.reveal(".footer__logo", { delay: 400, origin: "left" });
+sr.reveal(".footer__content", { delay: 700, origin: "left" });
+sr.reveal(".copyright", { dealy: 1200, origin: "bottom" });
